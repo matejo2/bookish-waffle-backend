18268264d2c32ec28145e0181fc993edf3e924c1
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-04-18 19:32:36 +0200

    added first green test to controller

---
+++ src/test/groovy/com/example/userbackend/ControllerTest.groovy
@@ -0,0 +1,34 @@
+package com.example.userbackend
+
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+
+@WebMvcTest
+@AutoConfigureWebMvc
+class ControllerTest extends Specification {
+
+    @Autowired
+    private MockMvc mockMvc
+
+    @SpringBean
+    private UserService service = Mock()
+
+    def "getting users works"() {
+        given: "an uri"
+        def uri = "/users"
+
+        when: "controller is called"
+        def response = mockMvc.perform(get(uri)).andReturn().response
+
+        then: "status is ok"
+        response.getStatus() == HttpStatus.OK.value()
+        and: "service was called"
+    }
+}
